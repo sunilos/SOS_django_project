818b706d1ce8286551875a71b4e4c4d91302a159
sahumayank <42769416+sahumayank@users.noreply.github.com>
2020-03-21 19:37:56 +0530

    Update CollegeCtl.py

--- ORS/ctl/CollegeCtl.py
+++ ORS/ctl/CollegeCtl.py
@@ -1,17 +1,20 @@
+
 from django.http import HttpResponse
 from .BaseCtl import BaseCtl
+from django.shortcuts import render
 
 class CollegeCtl(BaseCtl):
     def __init__(self):
         self.name = ""
         self.address = ""
+    
+    def preload(self,request):
+         self.preloadData=[{"name":"Madhya Pradesh","code":"MP"},{"name":"Uttar Pradesh","code":"UP"}]
+        
 
     def display(self,request,params={}):
-        return HttpResponse("This is College display")  
+        print(self.preloadData)
+        return render(request,"College.html",{"stateList":self.preloadData})  
 
     def submit(self,request,params={}):
         return HttpResponse("This is College submit")  
-        
-
-
-
