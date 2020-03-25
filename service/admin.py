829deed907c38712c176419c41aba6abfe585caa
sahumayank <42769416+sahumayank@users.noreply.github.com>
2020-03-25 13:15:25 +0530

    Update admin.py

--- service/admin.py
+++ service/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from . models import User, Role, College,Course,Marksheet,student, subject, timeTable
+from . models import User, Role, College,Course,Marksheet,Student, Subject, TimeTable
 
 admin.site.register(Role)
 admin.site.register(User)
 admin.site.register(College)
 admin.site.register(Course)
 admin.site.register(Marksheet)
-admin.site.register(student)
-admin.site.register(subject)
-admin.site.register(timeTable)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(TimeTable)
