2e22479eb1e150ed76f4668c6a31df67a2fb42a8
sahumayank <42769416+sahumayank@users.noreply.github.com>
2020-04-01 15:54:44 +0530

    Update views.py

--- ORSAPI/views.py
+++ ORSAPI/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from ORSAPI.restctl.CollegeListCtl import CollegeListCtl
 
 # Create your views here.
 def info(request,page,action ):
