863cdda30002c72d58ca474c8c21ad44ea121992
sahumayank <42769416+sahumayank@users.noreply.github.com>
2020-04-02 15:17:18 +0530

    Add files via upload

---
+++ ORSAPI/restctl/CollegeCtl.py
@@ -0,0 +1,41 @@
+
+from django.http import HttpResponse
+from .BaseCtl import BaseCtl
+from django.shortcuts import render
+from ORSAPI.utility.DataValidator import DataValidator
+from service.models import College
+from service.service.CollegeService import CollegeService
+from rest_framework.parsers import JSONParser
+from service.Serializers import CollegeSerializers
+
+class CollegeCtl(BaseCtl):    
+    def preload(self,request):
+         self.preloadData=[{"name":"Madhya Pradesh","code":"MP"},{"name":"Uttar Pradesh","code":"UP"}]
+        
+    #Display College page 
+    def display(self,request,params={}):
+        if( params["id"] > 0):
+            r = self.get_service().get(params["id"])
+            self.model_to_form(r)
+        res = render(request,self.get_template(), {"form":self.form})
+        return res
+
+    #Submit College page
+    def submit(self,request,params={}):
+        rr=JSONParser().parse(request)
+        print("qqqqqqqqqqqqqq==========",rr)
+        dd=CollegeSerializers(data=rr)
+        self.get_service().save(dd)
+        res = render(request,self.get_template(),{"form":self.form})
+        return res
+        
+    # Template html of Role page    
+    def get_template(self):
+        return "orsapi/College.html"          
+
+    # Service of Role     
+    def get_service(self):
+        return CollegeService()        
+
+
+
