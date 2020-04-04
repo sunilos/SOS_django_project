db4031224503ade59fed026c561dabfa18c24811
sahumayank <42769416+sahumayank@users.noreply.github.com>
2020-04-04 20:31:10 +0530

    Update urls.py

--- ORSAPI/urls.py
+++ ORSAPI/urls.py
@@ -18,8 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<page>/<action>/', views.action),       
-    path('<page>/<int:id>', views.actionId),       
-    path('<page>/', views.actionId),       
-
+      path('<page>/<action>/', views.action),       
+      path('<page>/<action>/<int:id>', views.action),   
 ]
