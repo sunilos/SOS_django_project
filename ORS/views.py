130a0834219db7c79a077da84730b158e35ef887
sahumayank <42769416+sahumayank@users.noreply.github.com>
2021-03-27 11:17:02 +0530

    Update views.py

--- ORS/views.py
+++ ORS/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 #Import controller classes
