1ab091e08416b2696d0493a9ea1e2e606bd6307a
Sunil Sahu <sunil.sahu@nenosystems.com>
2026-04-30 14:01:04 +0530

    User module corrcted

--- service/service/UserService.py
+++ service/service/UserService.py
@@ -2,37 +2,39 @@ from service.models import User
 from service.utility.DataValidator import DataValidator
 from .BaseService import BaseService
 
-'''
+"""
 It contains User business logics.   
-'''
+"""
+
+
 class UserService(BaseService):
 
-    def authenticate(self,params):
+    def authenticate(self, params):
         userList = self.search(params)
-        if (userList.count() > 0):
+        if userList.count() > 0:
             print("8888888->", userList[0].login)
             return userList[0]
         else:
             return None
-     
-    def search(self,params):
+
+    def search(self, params):
         q = self.get_model().objects.filter()
 
-        val = params.get("firstName",None)
-        if( DataValidator.isNotNull(val)):
-            q= q.filter( firstName = val)
+        val = params.get("firstName", None)
+        if DataValidator.isNotNull(val):
+            q = q.filter(firstName__icontains=val)
 
-        val = params.get("lastName",None)
-        if( DataValidator.isNotNull(val)):
-            q= q.filter( lastName = val)
+        val = params.get("lastName", None)
+        if DataValidator.isNotNull(val):
+            q = q.filter(lastName__icontains=val)
 
-        val = params.get("login",None)
-        if( DataValidator.isNotNull(val)):
-            q= q.filter( login = val)
+        val = params.get("login", None)
+        if DataValidator.isNotNull(val):
+            q = q.filter(login=val)
 
-        val = params.get("password",None)
-        if( DataValidator.isNotNull(val)):
-            q= q.filter( password = val)
+        val = params.get("password", None)
+        if DataValidator.isNotNull(val):
+            q = q.filter(password=val)
 
         return q
 
