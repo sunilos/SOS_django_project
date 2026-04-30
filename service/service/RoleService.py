c0eb4132e209f1d29987ce2a0efd054d3c61f708
Sunil Sahu <sunil.sahu@nenosystems.com>
2026-04-30 14:04:50 +0530

    User module corrcted

--- service/service/RoleService.py
+++ service/service/RoleService.py
@@ -10,13 +10,13 @@ class RoleService(BaseService):
     def search(self,params):
         q = self.get_model().objects.filter()
 
-        val = params.get("name",None)
-        if( DataValidator.isNotNull(val)):
-            q= q.filter( name = val)
+        val = params.get("name", None)
+        if DataValidator.isNotNull(val):
+            q = q.filter(name__icontains=val)
 
-        val = params.get("description",None)
-        if( DataValidator.isNotNull(val)):
-            q= q.filter( description = val)
+        val = params.get("description", None)
+        if DataValidator.isNotNull(val):
+            q = q.filter(description__icontains=val)
 
         return q
 
