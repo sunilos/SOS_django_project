b6bf844bf97bd8fdd16f94c5abf59c04f5cc9cc6
Sunil Sahu <sunil.sahu@nenosystems.com>
2026-05-06 10:46:10 +0530

    Multiple db added

--- SOS_django_projects/settings.py
+++ SOS_django_projects/settings.py
@@ -103,9 +103,27 @@ EMAIL_HOST_PASSWORD = 'password'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    },
+    # MySQL Database
+    "mysql_db": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ORSDJANGO",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
+    },
+    # PostgreSQL Database
+    "postgres_db": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ORSDJANGO",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
